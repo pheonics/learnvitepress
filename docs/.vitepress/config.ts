10796250c82940e4f60d4fb6e95ba09e61768551
IPPS Cloud Admin <cloudadmin@ipps.co.th>
2024-09-16 21:47:11 +0700

    Initial Commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'My Documentation',
+  description: 'A VitePress Site',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/api-examples' }
+    ],
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,9 @@
+# Welcome to My Documentation
+
+This is a paragraph in my documentation.
+
+## Features
+
+- Feature 1
+- Feature 2
+- Feature 3
